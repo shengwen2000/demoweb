c29bc6d3a360abf89d6ed020090241392897d8b6
shengwen <shengwen2000@gmail.com>
2019-07-06 00:44:39 +0800

    fix ng-include

--- src/web001/Scripts/webapp.demo/demo003.ts
+++ src/web001/Scripts/webapp.demo/demo003.ts
@@ -2,7 +2,9 @@
     Title: string;
     Active?: boolean;
     Content?: any;
+    Kind?: TabKind;
 }
+
 
 
 /**
@@ -34,13 +36,66 @@ class MainCtrl  {
 }
 
 
+type TabKind = "item" | "list";
+
+interface IRecord {
+    Id: string;
+    No: string;
+    Name: string;
+}
+
+
+
+class MainCtrl2 {
+
+    Tabs: ITab[] = [];
+
+    constructor(public $scope: ng.IScope, public $q: ng.IQService) {
+    }
+
+    tabClick(tab: ITab) {
+        for (let t of this.Tabs) {
+            t.Active = (t == tab);
+        }
+    }
+
+    tabAdd(title: string, kind : TabKind) {
+        if (!title) {
+            window.alert("title is required");
+            return;
+        }
+        
+        let n: ITab = { Title: title, Kind: kind };
+        if (kind == "item") {
+            n.Content = {
+                Id: '1', No:'001', Name: 'david'
+            } as IRecord
+        }
+        else if (kind == "list") {
+            n.Content = [
+                {
+                    Id: '1', No: '001', Name: 'david'
+                },
+                {
+                    Id: '2', No: '002', Name: 'kevin'
+                },
+            ] as IRecord[];
+        }
+
+        this.Tabs.push(n);
+        this.tabClick(n);
+    }
+}
+
+
 /**
  * start angular app
  */
 export function startApp() {
     angular.module("app", []);
     angular.module("app")      
-        .controller('MainCtrl', MainCtrl)        
+        .controller('MainCtrl', MainCtrl)    
+        .controller('MainCtrl2', MainCtrl2)        
         ;
     angular.bootstrap(document, ['app']);
 };
--- src/web001/wwwroot/Scripts/webapp.demo/demo003.js
+++ src/web001/wwwroot/Scripts/webapp.demo/demo003.js
@@ -27,13 +27,52 @@ define(["require", "exports"], function (require, exports) {
         };
         return MainCtrl;
     }());
+    var MainCtrl2 = /** @class */ (function () {
+        function MainCtrl2($scope, $q) {
+            this.$scope = $scope;
+            this.$q = $q;
+            this.Tabs = [];
+        }
+        MainCtrl2.prototype.tabClick = function (tab) {
+            for (var _i = 0, _a = this.Tabs; _i < _a.length; _i++) {
+                var t = _a[_i];
+                t.Active = (t == tab);
+            }
+        };
+        MainCtrl2.prototype.tabAdd = function (title, kind) {
+            if (!title) {
+                window.alert("title is required");
+                return;
+            }
+            var n = { Title: title, Kind: kind };
+            if (kind == "item") {
+                n.Content = {
+                    Id: '1', No: '001', Name: 'david'
+                };
+            }
+            else if (kind == "list") {
+                n.Content = [
+                    {
+                        Id: '1', No: '001', Name: 'david'
+                    },
+                    {
+                        Id: '2', No: '002', Name: 'kevin'
+                    },
+                ];
+            }
+            this.Tabs.push(n);
+            this.tabClick(n);
+        };
+        return MainCtrl2;
+    }());
     /**
      * start angular app
      */
     function startApp() {
         angular.module("app", []);
         angular.module("app")
-            .controller('MainCtrl', MainCtrl);
+            .controller('MainCtrl', MainCtrl)
+            .controller('MainCtrl2', MainCtrl2);
         angular.bootstrap(document, ['app']);
     }
     exports.startApp = startApp;
