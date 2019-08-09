e4b3093bed08f41aeffc41a8b0de0ab2b695ccd8
shengwen <shengwen2000@gmail.com>
2019-08-09 21:57:06 +0800

    fix

--- src/web001/Controllers/HelloController.cs
+++ src/web001/Controllers/HelloController.cs
@@ -20,9 +20,9 @@ namespace web001.Controllers
         [HttpPost]
         public IActionResult Index(HelloViewModel model)
         {
-            //mode.Name 由於 view 綁定輸入Input 會有保護禁止修改。 下達 ModelState.Clear()才可異動。
+            //mode.Name 由於 view 綁定輸入Input 會有保護禁止修改ㄥ。 下達 ModelState.Clear()才可異動。
             model.Name = DateTime.Now.ToString();
-            ModelState.Clear(); 清除輸入保護
+            ModelState.Clear(); //清除輸入保護
 
             //model.Note 由於 view 僅是輸出所以可隨時改
             model.Note = DateTime.Now.ToString();
