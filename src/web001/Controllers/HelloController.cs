fe780633f85bcdfef92ee0f2bed523ad347c9b58
shengwen <shengwen2000@gmail.com>
2019-08-09 21:53:35 +0800

    add HelloController example

---
+++ src/web001/Controllers/HelloController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace web001.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            var m = new HelloViewModel();
+            m.Name = "Kevin";
+            m.Note = "this is a note";
+            return View(m);
+        }
+
+        [HttpPost]
+        public IActionResult Index(HelloViewModel model)
+        {
+            //mode.Name 由於 view 綁定輸入Input 會有保護禁止修改。 下達 ModelState.Clear()才可異動。
+            model.Name = DateTime.Now.ToString();
+            ModelState.Clear(); 清除輸入保護
+
+            //model.Note 由於 view 僅是輸出所以可隨時改
+            model.Note = DateTime.Now.ToString();
+            return View(model);
+        }
+    }
+}
---
+++ src/web001/Models/HelloViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class HelloViewModel
+    {
+        [Required]
+        [MaxLength(100)]
+        [Display(Name ="Your Name")]
+        public string Name { get; set; }
+
+        public string Note { get; set; }
+    }
+}
